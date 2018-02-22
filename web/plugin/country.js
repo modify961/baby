12c0d070c42575b267714ad981aac339a1e0f30c
lijw <modify961@126.com>
2018-02-22 15:20:45 +0800

    beifen

---
+++ web/plugin/country.js
@@ -0,0 +1,36 @@
+﻿$(document).ready(function () {
+    var li = '<li>{0}</li>';
+    var top = ' <div>< div class="category" >'
+        + '<div class="category-img">'
+        + '<img src="images/cat1.png" title="image" alt="" /></div>'
+        + '<div class="category-info">'
+        + '<h4>{0}</h4>'
+        + '<span>{1} 个品牌</span>'
+        + '</div>'
+        + '<div class="clearfix"></div>'
+        + '</div >'
+        + '<div class="sub-categories">'
+        + '<ul>{2}'
+        + '<div class="clearfix"></div>'
+        + '</ul>'
+        +'</div></div >';
+    $.ajax({
+        type: "GET",
+        url: "../json/brand.json",
+        dataType: "json",
+        success: function (data) {
+            $(data).each(function (_, flag) {
+                var item = li.replace("{1}", flag.country);
+                $(item).appendTo($("#resp-tabs-list"));
+                var topitem = top.replace("{0}", flag.country);
+                topitem = topitem.replace("{1}", flag.brand.length);
+                var lis = "";
+                $(flag.brand).each(function (i,n) {
+                    lis = lis + '<li><a href="mobiles.html">' + n.name+'</a></li>' ;
+                })
+                topitem = topitem.replace("{2}", lis);
+                $(topitem).appendTo($("#resp-tabs"));
+            })
+        }
+    });
+})
--- web/plugin/index.js
+++ web/plugin/index.js
@@ -1,1 +1,69 @@
-﻿
+﻿$(document).ready(function () {
+    var html = '<div class="col-md-2 focus-grid">'
+        + '<a href= "country.html" >'
+        + '<div class="focus-border">'
+        + '<div class="focus-layout">'
+        + '<div class="focus-image"><i class="fa {0}"></i></div>'
+        + '<h4 class="clrchg">{1}</h4>'
+        + '</div>'
+        + '</div></a ></div >'; 
+    var top = '<div class="col-md-3 biseller-column">'
+        + '<a href= "single.html" >'
+        + '<img src="img/{3}" />'
+        + '<span class="price">&#36; {0}</span></a >'
+        + '<div class="ad-info">'
+        + '<h5>{1}</h5>'
+        + '<span>{2}</span></div></div >';
+    $.ajax({
+        type: "GET",
+        url: "../json/country.json",
+        dataType: "json",
+        success: function (data) {
+            $(data).each(function (_, flag) {
+                var item = html.replace("{0}", flag.icon).replace("{1}", flag.name);
+                $(item).appendTo($("#container"));
+            })
+        }
+    });
+    $.ajax({
+        type: "GET",
+        url: "../json/recommend.json",
+        dataType: "json",
+        success: function (data) {
+            var li = "<li >";
+            $(data).each(function (_, flag) {
+                debugger
+                var item = top.replace("{0}", flag.price).replace("{1}", flag.remark)
+                    .replace("{2}", flag.updateTime).replace("{3}", flag.img);
+                if (_ % 4 == 0) {
+                    $(li+"</li>").appendTo($("#flexiselDemo3"));
+                    li = "<li >";
+                }
+                li = li + item;
+            })
+
+            $("#flexiselDemo3").flexisel({
+                visibleItems: 1,
+                animationSpeed: 1000,
+                autoPlay: true,
+                autoPlaySpeed: 5000,
+                pauseOnHover: true,
+                enableResponsiveBreakpoints: true,
+                responsiveBreakpoints: {
+                    portrait: {
+                        changePoint: 480,
+                        visibleItems: 1
+                    },
+                    landscape: {
+                        changePoint: 640,
+                        visibleItems: 1
+                    },
+                    tablet: {
+                        changePoint: 768,
+                        visibleItems: 1
+                    }
+                }
+            });
+        }
+    });
+})
